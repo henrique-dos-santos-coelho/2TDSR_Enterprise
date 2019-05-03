d4783ddfcd4c37d14c714276c9e614aa5aed2bb9
henriquesantoscoelho <rike.coelho22@gmail.com>
2019-05-02 21:42:36 -0300

    commit

--- 08-JPA-Mapeamento-Avancado-Ultimato/src/br/com/fiap/dao/ConsultaDAO.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.dao;
-
-import br.com.fiap.entity.Consulta;
-import br.com.fiap.entity.ConsultaPK;
-
-public interface ConsultaDAO 
-				extends GenericDAO<Consulta, ConsultaPK> {
-
-}
--- 08-JPA-Mapeamento-Avancado-Ultimato/src/br/com/fiap/dao/PessoaDAO.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.dao;
-
-import br.com.fiap.entity.Pessoa;
-
-public interface PessoaDAO extends GenericDAO<Pessoa, Integer>{
-
-}
